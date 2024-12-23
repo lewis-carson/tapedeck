efddb532c7276552728f4a0d427cbc597fa820cf
Lewis Carson <lcarson679@wallacehigh.net>
2024-12-23 19:47:13 +0000

    final interleaver

--- interleaver.py
+++ interleaver.py
@@ -23,19 +23,12 @@ def main():
     
     # Initialize data from all generators
     line_data = [next(gen) for gen in generators]
-    diff = []
 
     while any(data is not None for data in line_data):
         times = [line['receive_time'] if line else float('inf') for line in line_data]
         smallest_time_index = times.index(min(times))
 
-        if line_data[smallest_time_index]["is_partial"]:
-            diff.append(line_data[smallest_time_index]["receive_time"] - 
-                       line_data[smallest_time_index]["order_book"]["E"])
-
-        recv_time = line_data[smallest_time_index]["receive_time"]
-        symbol = line_data[smallest_time_index]["symbol"]
-        print(f"{recv_time} {symbol}")
+        print(line_data[smallest_time_index])
 
         # Get next line from the generator that had the smallest time
         line_data[smallest_time_index] = next(generators[smallest_time_index])
