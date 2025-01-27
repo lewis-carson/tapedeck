ef1113205a87c3a0a3d4b2ad71d7935ee47b16e8
Lewis Carson <lcarson679@wallacehigh.net>
2025-01-27 14:03:15 +0000

    rewrite world

--- watch/src/app.rs
+++ watch/src/app.rs
@@ -13,55 +13,86 @@ use std::{
     collections::HashMap,
     fs::File,
     io::{self, BufRead, BufReader},
-    sync::mpsc::channel,
+    sync::mpsc::{Receiver, channel},
     thread,
     time::Duration,
 };
 
-/*
-let (tx, rx) = channel();
-
-// spawn a thread to read from /dev/stdin
-// entering raw mode in crossterm seems to break std::io::stdin
-thread::spawn(move || {
-    let file = File::open("/dev/stdin").unwrap();
-    for line in BufReader::new(file).lines() {
-        tx.send(line.unwrap()).ok();
-    }
-}); */
-
 #[derive(Debug)]
-enum StreamObject {
-    Event(datatypes::Event),
-    World(datatypes::world_builder::World),
-}
-
-#[derive(Debug, Default)]
 pub struct App {
-    /// Is the application running?
     running: bool,
-    streams: HashMap<String, Vec<StreamObject>>,
-    streams_channels: HashMap<String, std::sync::mpsc::Receiver<StreamObject>>,
+
+    partials: Vec<datatypes::Event>,
+    fulls: Vec<datatypes::Event>,
+    orders: Vec<datatypes::Event>,
+    fills: Vec<datatypes::Event>,
+    worlds: Vec<datatypes::world_builder::World>,
+
+    event_stream: Receiver<datatypes::Event>,
+    world_stream: Receiver<datatypes::world_builder::World>,
 }
 
 impl App {
     /// Construct a new instance of [`App`].
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            running: true,
+            partials: Vec::new(),
+            fulls: Vec::new(),
+            orders: Vec::new(),
+            fills: Vec::new(),
+            worlds: Vec::new(),
+            event_stream: channel().1,
+            world_stream: channel().1,
+        }
     }
 
-    fn new_stream(
-        &mut self,
-        name: &str,
-        handle: impl Fn(&std::sync::mpsc::Sender<StreamObject>) + Send + 'static,
-    ) {
-        let (tx, rx) = channel();
-        self.streams.insert(name.to_string(), Vec::new());
-        self.streams_channels.insert(name.to_string(), rx);
+    /// Run the application's main loop.
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        terminal.clear().unwrap();
 
-        thread::spawn(move || {
-            handle(&tx);
-        });
+        // Flatten the new_stream logic for "fulls"
+        {
+            let (tx, rx) = channel();
+            self.event_stream = rx;
+
+            thread::spawn(move || {
+                let command = "tail -fq data/*";
+                for line in Self::launch_command(command) {
+                    let line: datatypes::Event = serde_json::from_str(&line).unwrap();
+
+                    tx.send(line).ok();
+                }
+            });
+        }
+
+        // Flatten the new_stream logic for "world"
+        {
+            let (tx, rx) = channel();
+            self.world_stream = rx;
+
+            thread::spawn(move || {
+                let command = "tail -fq data/*";
+
+                let world_builder = datatypes::world_builder::WorldBuilder::new(Box::new(
+                    Self::launch_command(command).map(|line| {
+                        let event: datatypes::Event = serde_json::from_str(&line).unwrap();
+                        Ok(event)
+                    }),
+                ));
+
+                for world in world_builder {
+                    tx.send(world.unwrap()).ok();
+                }
+            });
+        }
+
+        self.running = true;
+        while self.running {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_crossterm_events()?;
+        }
+        Ok(())
     }
 
     fn launch_command(command: &str) -> impl Iterator<Item = String> {
@@ -74,70 +105,6 @@ impl App {
 
         let reader = BufReader::new(child.stdout.unwrap());
         reader.lines().map(|line| line.unwrap())
-    }
-
-    /// Run the application's main loop.
-    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        terminal.clear();
-
-        self.new_stream("fulls", |tx| {
-            // launch a command and read from its stdout
-            let command = "tail -fq data/*";
-            for line in Self::launch_command(command) {
-                let line: datatypes::Event = serde_json::from_str(&line).unwrap();
-
-                if let datatypes::EventType::FullOrderBook(_) = line.event {
-                    tx.send(StreamObject::Event(line)).ok();
-                }
-            }
-        });
-
-        self.new_stream("partials", |tx| {
-            // launch a command and read from its stdout
-            let command = "tail -fq data/*";
-            for line in Self::launch_command(command) {
-                let line: datatypes::Event = serde_json::from_str(&line).unwrap();
-
-                if let datatypes::EventType::PartialOrderBook(_) = line.event {
-                    tx.send(StreamObject::Event(line)).ok();
-                }
-            }
-        });
-
-        self.new_stream("open_orders", |tx| {
-            // launch a command and read from its stdout
-            let command = "tail -fq data/*";
-            for line in Self::launch_command(command) {
-                let line: datatypes::Event = serde_json::from_str(&line).unwrap();
-
-                if let datatypes::EventType::OrderTradeEvent(_) = line.event {
-                    tx.send(StreamObject::Event(line)).ok();
-                }
-            }
-        });
-
-        self.new_stream("world", |tx| {
-            // launch a command and read from its stdout
-            let command = "tail -fq data/*";
-
-            let world_builder = datatypes::world_builder::WorldBuilder::new(Box::new(
-                Self::launch_command(command).map(|line| {
-                    let event: datatypes::Event = serde_json::from_str(&line).unwrap();
-                    Ok(event)
-                }),
-            ));
-
-            for world in world_builder {
-                tx.send(StreamObject::World(world.unwrap())).ok();
-            }
-        });
-
-        self.running = true;
-        while self.running {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_crossterm_events()?;
-        }
-        Ok(())
     }
 
     fn timestamp_to_string(timestamp: u64) -> String {
@@ -155,14 +122,29 @@ impl App {
     /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
     /// - <https://github.com/ratatui/ratatui/tree/master/examples>
     fn draw(&mut self, frame: &mut Frame) {
-        for (name, rx) in self.streams_channels.iter() {
-            if let Ok(line) = rx.try_recv() {
-                self.streams.get_mut(name).unwrap().push(line);
+        for event in self.event_stream.try_iter() {
+            match event.event {
+                EventType::PartialOrderBook(_) => {
+                    self.partials.push(event);
+                }
+                EventType::FullOrderBook(_) => {
+                    self.fulls.push(event);
+                }
+                EventType::OrderTradeEvent(_) => {
+                    self.orders.push(event);
+                }
+                EventType::AccountInformation(_) => {}
+                EventType::OpenOrders(_) => {}
             }
         }
 
         // cut streams down to 100 elements
-        for (_, stream) in self.streams.iter_mut() {
+        for (_, stream) in &mut [
+            ("partials", &mut self.partials),
+            ("fulls", &mut self.fulls),
+            ("open_orders", &mut self.orders),
+            ("fills", &mut self.fills),
+        ] {
             if stream.len() > 100 {
                 stream.drain(0..stream.len() - 100);
             }
@@ -170,44 +152,28 @@ impl App {
 
         let master_layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints(vec![
-                Constraint::Length(3),
-                Constraint::Fill(1),
-            ])
+            .constraints(vec![Constraint::Length(3), Constraint::Fill(1)])
             .split(frame.area());
 
         let main_layout = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Fill(1),
-                Constraint::Fill(3),
-            ])
+            .constraints(vec![Constraint::Fill(1), Constraint::Fill(3)])
             .split(master_layout[1]);
 
         let main_right = Layout::default()
             .direction(Direction::Vertical)
-            .constraints(vec![
-                Constraint::Fill(1),
-                Constraint::Fill(1),
-            ])
+            .constraints(vec![Constraint::Fill(1), Constraint::Fill(1)])
             .split(main_layout[1]);
 
         let main_right_top = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Ratio(1, 2),
-                Constraint::Ratio(1, 2),
-            ])
+            .constraints(vec![Constraint::Ratio(1, 2), Constraint::Ratio(1, 2)])
             .split(main_right[0]);
 
         let main_right_bottom = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Ratio(1, 2),
-                Constraint::Ratio(1, 2),
-            ])
+            .constraints(vec![Constraint::Ratio(1, 2), Constraint::Ratio(1, 2)])
             .split(main_right[1]);
-
 
         let header_area = master_layout[0];
         let world_area = main_layout[0];
@@ -227,146 +193,113 @@ impl App {
 
         // if world stream has some length, destructure it to get the last element
         let world = self
-            .streams
-            .get("world")
-            .unwrap()
+            .worlds
             .last()
             .map(|world| {
-                if let StreamObject::World(world) = world {
-                    world.account_information.clone().map(|account| {
+                world
+                    .account_information
+                    .clone()
+                    .map(|account| {
                         account
                             .balances
                             .into_iter()
                             .map(|balance| format!("{}: {}", balance.asset, balance.free))
                             .collect::<Vec<_>>()
                             .join("\n")
-                    }).unwrap_or("".to_string())
-                } else {
-                    "".to_string()
-                }
+                    })
+                    .unwrap_or("No balances in world".to_string())
             })
-            .unwrap_or("".to_string());
+            .unwrap_or("No world yet".to_string());
 
         frame.render_widget(
             Paragraph::new(world).block(Block::bordered().title("World")),
             world_area,
         );
 
+        let mut opened = vec![];
+        let mut filled = vec![];
 
-        let open_orders = self
-            .streams
-            .get("open_orders")
-            .unwrap()
-            .iter()
-            .filter_map(|stream| {
-                let event = if let StreamObject::Event(event) = stream {
-                    event
-                } else {
-                    panic!("Expected Event")
-                };
+        let orders = self.orders.iter().for_each(|event| {
+            if let EventType::OrderTradeEvent(order_event) = &event.event {
+                let exec = order_event.execution_type.clone();
 
-                if let EventType::OrderTradeEvent(order) = &event.event {
-                    let time = Self::timestamp_to_string(event.receive_time);
-                    Some(format!(
-                        "{} | {} | {} | {} | {}",
-                        time,
-                        order.symbol,
-                        order.side,
-                        order.price,
-                        order.qty,
-                    ))
+                if exec == "NEW" {
+                    opened.push(format!("{} | {}", Self::timestamp_to_string(event.receive_time), event.symbol));
                 } else {
-                    panic!("Expected OrderTradeEvent")
+                    filled.push(format!("{} | {}", Self::timestamp_to_string(event.receive_time), event.symbol));
                 }
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
+            }
+        });
 
-        let open_orders_offset = self
-            .streams
-            .get("open_orders")
-            .unwrap()
+        let opened_offset = opened
             .len()
             .saturating_sub(orders_area.height as usize - 2);
 
         frame.render_widget(
-            Paragraph::new(open_orders)
+            Paragraph::new(opened.join("\n"))
                 .block(Block::bordered().title("Orders"))
-                .scroll((open_orders_offset as u16, 0)),
+                .scroll((opened_offset as u16, 0)),
             orders_area,
         );
 
+        let fills_offset = filled
+            .len()
+            .saturating_sub(fills_area.height as usize - 2);
+
         frame.render_widget(
-            Paragraph::new("")
+            Paragraph::new(filled.join("\n"))
                 .block(Block::bordered().title("Fills"))
-                .centered(),
+                .scroll((fills_offset as u16, 0)),
             fills_area,
         );
 
         // map all of fulls to FullOrderBook vector
         let fulls = self
-            .streams
-            .get("fulls")
-            .unwrap()
+            .fulls
             .iter()
-            .filter_map(|stream| {
-                if let StreamObject::Event(event) = stream {
-                    let time = Self::timestamp_to_string(event.receive_time);
-                    Some(format!("{} | {}", time, event.symbol))
-                } else {
-                    None
-                }
+            .filter_map(|event| {
+                let time = Self::timestamp_to_string(event.receive_time);
+                Some(format!("{} | {}", time, event.symbol))
             })
-            .collect::<Vec<_>>()
-            .join("\n");
+            .collect::<Vec<_>>();
 
         let fulls_offset = self
-            .streams
-            .get("fulls")
-            .unwrap()
+            .fulls
             .len()
             .saturating_sub(fulls_area.height as usize - 2);
 
         frame.render_widget(
-            Paragraph::new(fulls)
+            Paragraph::new(fulls.join("\n"))
                 .scroll((fulls_offset as u16, 0))
                 .block(Block::bordered().title("Full Books")),
             fulls_area,
         );
 
         let partials = self
-            .streams
-            .get("partials")
-            .unwrap()
+            .partials
             .iter()
-            .filter_map(|stream| {
-                if let StreamObject::Event(event) = stream {
-                    let time = Self::timestamp_to_string(event.receive_time);
+            .filter_map(|event| {
+                let time = Self::timestamp_to_string(event.receive_time);
 
-                    let partial = if let EventType::PartialOrderBook(ob) = &event.event {
-                        ob
-                    } else {
-                        panic!("Expected PartialOrderBook event")
-                    };
-
-                    let bids_len = partial.bids.len();
-                    let asks_len = partial.asks.len();
-
-                    Some(format!(
-                        "{} | {:<10} | Bids: {:>4} | Asks: {:>4}",
-                        time, event.symbol, bids_len, asks_len
-                    ))
+                let partial = if let EventType::PartialOrderBook(ob) = &event.event {
+                    ob
                 } else {
-                    None
-                }
+                    panic!("Expected PartialOrderBook event")
+                };
+
+                let bids_len = partial.bids.len();
+                let asks_len = partial.asks.len();
+
+                Some(format!(
+                    "{} | {:<10} | Bids: {:>4} | Asks: {:>4}",
+                    time, event.symbol, bids_len, asks_len
+                ))
             })
             .collect::<Vec<_>>()
             .join("\n");
 
         let partials_offset = self
-            .streams
-            .get("partials")
-            .unwrap()
+            .partials
             .len()
             .saturating_sub(partials_area.height as usize - 2);
         frame.render_widget(
